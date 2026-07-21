79573a65820c3533407b29ee96e2e8bf092578db
Abdullah <141498185+sudo-rm-abdullah@users.noreply.github.com>
2026-07-21 05:18:13 -0700

    Temporary CORS test

--- backend/main.py
+++ backend/main.py
@@ -38,20 +38,21 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 
 
+app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-
     allow_origins=[
         "https://nextjs-todo-three-inky.vercel.app",
-        "http://localhost:3000"
     ],
-
     allow_credentials=True,
-
     allow_methods=["*"],
-
     allow_headers=["*"],
 )
+
+app.include_router(auth_router)
+app.include_router(todo.router)
+app.include_router(dashboard.router)
 
 
 
