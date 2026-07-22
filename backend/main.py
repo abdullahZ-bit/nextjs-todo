2627fb27719bc9c222f3668c55d282a92a780235
Abdullah <141498185+sudo-rm-abdullah@users.noreply.github.com>
2026-07-22 12:33:18 -0700

    Add regex to allow all Vercel preview deployment URLs

--- backend/main.py
+++ backend/main.py
@@ -44,9 +44,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://nextjs-todo-three-inky.vercel.app",
-         "http://localhost:3000",
-          "http://127.0.0.1:3000",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
     ],
+    allow_origin_regex=r"https://nextjs-todo-.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
