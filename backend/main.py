1be87f33b84b1d9b0a5e3499530abbc210e60ee5
Abdullah <141498185+sudo-rm-abdullah@users.noreply.github.com>
2026-07-22 12:13:13 -0700

    Fix hardcoded API URL and clean up CORS config

--- backend/main.py
+++ backend/main.py
@@ -44,6 +44,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://nextjs-todo-three-inky.vercel.app",
+         "http://localhost:3000",
+          "http://127.0.0.1:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
